4c0ffc04b23d1f05b000f37d84c1058bc73fe62d
nora <sxj042@gmail.com>
2023-04-19 22:24:25 -0400

    working on

---
+++ src/main/java/io/zipcoder/crudapp/PersonController.java
@@ -0,0 +1,32 @@
+package io.zipcoder.crudapp;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class PersonController {
+
+    public Person createPerson (Person p) {
+        return null;
+    }
+
+    public Person getPerson(int id) {
+        return null;
+    }
+
+    public List<Person> getPersonList() {
+        return null;
+    }
+
+    public Person updatePerson(Person p) {
+        return null;
+    }
+
+    public void DeletePerson(int id) {
+        
+    }
+    
+}
